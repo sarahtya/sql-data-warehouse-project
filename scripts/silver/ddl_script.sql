cc42e8f51eda37b48cd910ddf731b55b04ec4971
Sarah Tran <sarahtya1501@gmail.com>
2026-04-23 21:48:43 +1000

    Update ddl_script.sql

--- scripts/silver/ddl_script.sql
+++ scripts/silver/ddl_script.sql
@@ -31,15 +31,17 @@ GO
 
 CREATE TABLE silver.crm_prd_info (
     prd_id       INT,
+    cat_id       NVARCHAR(50),   
     prd_key      NVARCHAR(50),
     prd_nm       NVARCHAR(50),
     prd_cost     INT,
     prd_line     NVARCHAR(50),
-    prd_start_dt DATETIME,
-    prd_end_dt   DATETIME,
+    prd_start_dt DATE,
+    prd_end_dt   DATE,
     dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
+	
 
 IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
     DROP TABLE silver.crm_sales_details;
